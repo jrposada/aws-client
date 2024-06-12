98cb1dad8fb305a0df2f183521a8134fa702a5ac
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-06-12 18:44:56 +0200

    feat: aws credentials instructions and dummy command with aws profile

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod commands;
+mod services;
+
+use commands::dynamodb::list;
+
 fn main() {
     tauri::Builder
         ::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![dynamodb_list])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+async fn dynamodb_list(profile_name: &str) -> Result<String, ()> {
+    let _ = list(profile_name).await;
+    Ok(format!("Hello, {}!", profile_name))
 }
