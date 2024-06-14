0efc6e168cd432da9b2b3e4d2f7b6388b24b1d5e
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-06-14 19:24:00 +0200

    feat: add rds inputs

--- src/features/request-panel/rds-panel/rds-options.tsx
+++ src/features/request-panel/rds-panel/rds-options.tsx
@@ -1,7 +1,12 @@
-import { Box, Button } from '@mui/material';
-import { FunctionComponent, MouseEventHandler } from 'react';
+import { Box, Button, TextField, Toolbar } from '@mui/material';
+import {
+    ChangeEventHandler,
+    FunctionComponent,
+    MouseEventHandler,
+} from 'react';
 import { RdsRequest } from '../../../core/commands/rds';
 import TextEditor, { TextEditorProps } from '../../text-editor/text-editor';
+import { t } from 'i18next';
 
 type RdsOptionsProps = {
     request: RdsRequest;
@@ -12,23 +17,84 @@ const RdsOptions: FunctionComponent<RdsOptionsProps> = ({
     request,
     onSend,
 }) => {
+    const handleClusterArnChange: ChangeEventHandler<HTMLInputElement> = (
+        event,
+    ) => {
+        request.setData((prev) => ({
+            ...prev,
+            clusterArn: event.target.value,
+        }));
+    };
+
+    const handleDatabaseChange: ChangeEventHandler<HTMLInputElement> = (
+        event,
+    ) => {
+        request.setData((prev) => ({
+            ...prev,
+            database: event.target.value,
+        }));
+    };
+
+    const handleProfileNameChange: ChangeEventHandler<HTMLInputElement> = (
+        event,
+    ) => {
+        request.setData((prev) => ({
+            ...prev,
+            profileName: event.target.value,
+        }));
+    };
+
     const handleQueryChange: TextEditorProps['onChange'] = (next) => {
         request.setData((prev) => ({
             ...prev,
-
-            database: 'scon',
-            clusterArn:
-                'arn:aws:rds:us-east-1:220162591379:cluster:scon-test-supply-connections-db-cluster',
-            profileName: 'dsco',
             query: next,
-            secretArn:
-                'arn:aws:secretsmanager:us-east-1:220162591379:secret:scon-supply-connections-db-test-readonly-password-secret-zuWRhJ',
+        }));
+    };
+
+    const handleSecretArnChange: ChangeEventHandler<HTMLInputElement> = (
+        event,
+    ) => {
+        request.setData((prev) => ({
+            ...prev,
+            secretArn: event.target.value,
         }));
     };
 
     return (
         <>
-            <Box sx={{ display: 'flex' }}>
+            <Toolbar
+                sx={{
+                    gap: 1,
+                }}
+                style={{
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                }}
+            >
+                <TextField
+                    label={t('rds-request.database')}
+                    onChange={handleDatabaseChange}
+                    value={request.data.database}
+                    variant="outlined"
+                />
+                <TextField
+                    label={t('rds-request.cluster-arn')}
+                    onChange={handleClusterArnChange}
+                    value={request.data.clusterArn}
+                    variant="outlined"
+                />
+                <TextField
+                    label={t('rds-request.secret-arn')}
+                    onChange={handleSecretArnChange}
+                    value={request.data.secretArn}
+                    variant="outlined"
+                />
+                <TextField
+                    label={t('rds-request.profile-name')}
+                    onChange={handleProfileNameChange}
+                    value={request.data.profileName}
+                    variant="outlined"
+                />
                 <Button
                     variant="contained"
                     onClick={onSend}
@@ -45,7 +111,7 @@ const RdsOptions: FunctionComponent<RdsOptionsProps> = ({
                 >
                     Send
                 </Button>
-            </Box>
+            </Toolbar>
             <TextEditor
                 value={request.data.query ?? ''}
                 onChange={handleQueryChange}
@@ -56,3 +122,9 @@ const RdsOptions: FunctionComponent<RdsOptionsProps> = ({
 
 export default RdsOptions;
 export type { RdsOptionsProps };
+// database: 'scon',
+// clusterArn:
+//     'arn:aws:rds:us-east-1:220162591379:cluster:scon-test-supply-connections-db-cluster',
+// profileName: 'dsco',
+// secretArn:
+//     'arn:aws:secretsmanager:us-east-1:220162591379:secret:scon-supply-connections-db-test-readonly-password-secret-zuWRhJ',
