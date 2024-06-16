5e5cccd931fbf2e9015da4395e49a5fda3f0e845
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-06-16 18:20:21 +0200

    fix: fix tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,11 @@
 import { describe, expect, it, vi } from 'vitest';
 
 vi.mock('./app-layout', () => ({ AppLayout: () => {} }));
+vi.mock('./core/hooks/request-context/create-request-context', () => ({
+    useCreateRequestContext: () => ({
+        requestContext: { Provider: () => <></> },
+    }),
+}));
 
 import { render } from '@testing-library/react';
 import App from './app';
