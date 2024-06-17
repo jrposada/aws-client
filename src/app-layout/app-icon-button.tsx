6cc2c90ec9ce139f0b8dd4923669ddb7f7531aee
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-06-18 00:15:09 +0200

    feat: open file (#13)
    ## Summary
    Open file.

--- src/app-layout/app-icon-button.tsx
+++ src/app-layout/app-icon-button.tsx
@@ -1,15 +1,28 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import { MenuItem } from '@mui/material';
+import { Divider } from '@mui/material';
 import { t } from 'i18next';
 import { FunctionComponent, MouseEventHandler, useRef } from 'react';
+import { useRequestService } from '../core/hooks/request-context/use-request-service';
 import MenuButton from '../ui/menu-button/menu-button';
+import MenuButtonItem from '../ui/menu-button/menu-button-item';
 import { MenuButtonService } from '../ui/menu-button/menu-button-service';
 import AppBarIconButton from './styled/app-bar-icon-button';
-import { useRequestService } from '../core/hooks/request-context/use-request-service';
 
 const AppIconButton: FunctionComponent = () => {
     const menuButtonService = useRef<MenuButtonService>(null);
     const requestService = useRequestService();
+
+    const handleOpen: MouseEventHandler<HTMLLIElement> = () => {
+        requestService.open();
+    };
+
+    const handleSave: MouseEventHandler<HTMLLIElement> = () => {
+        if (requestService.filepath) {
+            requestService.save(requestService.filepath);
+        } else {
+            requestService.saveAs();
+        }
+    };
 
     const handleSaveAs: MouseEventHandler<HTMLLIElement> = () => {
         requestService.saveAs();
@@ -26,7 +39,17 @@ const AppIconButton: FunctionComponent = () => {
                 <MenuIcon />
             </AppBarIconButton>
             <MenuButton ref={menuButtonService}>
-                <MenuItem onClick={handleSaveAs}>{t('save-as')}</MenuItem>
+                {/* <MenuButtonItem onClick={handleSaveAs}>{t('new')}</MenuButtonItem> */}
+                <MenuButtonItem onClick={handleOpen}>
+                    {t('open')}
+                </MenuButtonItem>
+                <Divider />
+                <MenuButtonItem onClick={handleSave}>
+                    {t('save')}
+                </MenuButtonItem>
+                <MenuButtonItem onClick={handleSaveAs}>
+                    {t('save-as')}
+                </MenuButtonItem>
             </MenuButton>
         </>
     );
