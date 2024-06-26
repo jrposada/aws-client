67a999168cbab9b2ca1ce23cb9cec2415e3bd827
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-06-26 10:02:07 +0200

    feat: toast (#25)
    ## Summary
    Toast infrastructure.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { SnackbarProvider } from 'notistack';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './i18n';
 import { rootRoute } from './root-route';
 import { routes } from './routes';
+import ErrorSnackbar from './ui/snackbar/error-snackbar';
+import InfoSnackbar from './ui/snackbar/info-snackbar';
+import Snackbar from './ui/snackbar/snackbar';
+import SuccessSnackbar from './ui/snackbar/success-snackbar';
+import WarningSnackbar from './ui/snackbar/warning-snackbar';
 
 const defaultTheme = createTheme();
 
@@ -18,11 +24,22 @@ const router = createRouter({ routeTree });
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider theme={defaultTheme}>
-            <CssBaseline />
-            <RouterProvider
-                router={router}
-                basepath={import.meta.env.BASE_URL}
-            />
+            <SnackbarProvider
+                maxSnack={3}
+                Components={{
+                    default: Snackbar,
+                    error: ErrorSnackbar,
+                    info: InfoSnackbar,
+                    success: SuccessSnackbar,
+                    warning: WarningSnackbar,
+                }}
+            >
+                <CssBaseline />
+                <RouterProvider
+                    router={router}
+                    basepath={import.meta.env.BASE_URL}
+                />
+            </SnackbarProvider>
         </ThemeProvider>
     </React.StrictMode>,
 );
